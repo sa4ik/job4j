24e355100f8f076c5d5d09ad0c9790e642ee3952
sa4ik <sachavichka@gmail.com>
2019-01-07 20:25:27 +0100

    1.5.Chekstyle[#101768]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,13 +6,13 @@ package ru.job4j.calculate;
 * @autor Sergey Sachavichka (sachavichka@gmail.com)
 */
 
-public class Calculate{
+public class Calculate {
 	
 	/** Конструктор, вывод строки в консоль.
 	* @param args - args.
 	*/
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 	  System.out.println("Hello World");
 	}
 	
@@ -21,7 +21,7 @@ public class Calculate{
 	 *@param name Your name.
 	 *@return Echo plus name.
 	 */
-	public String echo(String name){
+	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	} 
 }
