9f487ae83eb7a05b2d5df5d09cb742e8b0802a09
sa4ik <sachavichka@gmail.com>
2019-01-23 20:06:38 +0100

    (Dummy Bot)

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+/**
+ *@author Sergey Sachavichka (sachavichka@gmail.com)
+ *@version $Id$
+ *@since 0.1
+ */
+ public class DummyBot {
+ /**
+  * Отвечает на вопросы.
+  * @param question Вопрос от клиента.
+  * @return Ответ.
+  */
+  public String answer(String question) {
+	 String rs1 = "Это ставит меня в тупик. Спросите другой вопрос";
+	 if ("Привет, Бот.".equals(question)) {
+	     rs1 = "Привет, Умник";
+     } else if ("Пока".equals(question)){
+         rs1 = "До скорой встречи";
+     }
+     return rs1;
+   }
+  
+  }
+ 
+ 
+ 
