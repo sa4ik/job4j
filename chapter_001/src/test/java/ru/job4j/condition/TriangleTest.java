7bd24dae4bde9292321683427c09e28b29c50449
sa4ik <sachavichka@gmail.com>
2019-01-28 20:17:23 +0100

    (Площадь треугольника и тест)

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+	/**
+	 *@author Sergey Sachavichka (sachavichka@gmail.com)
+	 *@version $Id$
+	 *@since 0.1
+	 */
+	@Test
+	public void whenAreaSetThreePointsThenTriangleArea() {
+		Point a = new Point(0, 0);
+		Point b = new Point(0, 2);
+		Point c = new Point(2, 0);
+		Triangle triangle = new Triangle(a, b, c);
+		double result = triangle.area();
+		double expected = 2D;
+		assertThat(result, closeTo(expected, 0.1));
+	}
+}
