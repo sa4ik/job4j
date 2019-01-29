ad100af877724e1a149a7ce46dac0bd5cf1008b0
sa4ik <sachavichka@gmail.com>
2019-01-29 19:55:48 +0100

    (max)

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -8,11 +8,11 @@ package ru.job4j.max;
  public class Max {
  	/**
   * Находит максимальное из двух чисел.
-  * @param first and second.
+  * @param first and second and third.
   * @return maximum.
   */
 	 public int max(int first, int second) {
-		 return first > second ? first : second;
+		 return Math.max(first, second);
 
 	 }
 
