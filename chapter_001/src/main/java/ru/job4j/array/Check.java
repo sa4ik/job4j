dda94b694deca94de219a8ecea8c2cb02a72446f
sa4ik <sachavichka@gmail.com>
2019-03-13 20:16:08 +0100

    (Check array)

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ *@author Sergey Sachavichka (sachavichka@gmail.com)
+ *@version $Id$
+ *@since 0.1
+ */
+ 
+ public class Check {
+	 public boolean mono(boolean[] data) {
+		 boolean result = false;
+		   for (int index = 0; index < data.length; index++) {
+			  if (data[index] == true) {
+			  	result = true;}
+			  }
+		   return result;
+
+	 }
+
+ }
