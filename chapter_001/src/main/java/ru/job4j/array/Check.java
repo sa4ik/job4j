7b0beb93e8958fc668a9bb712450d84bb1805e47
sa4ik <sachavichka@gmail.com>
2019-04-04 21:16:52 +0200

    проверка массива

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -9,12 +9,15 @@ package ru.job4j.array;
  public class Check {
 	 public boolean mono(boolean[] data) {
 		 boolean result = false;
-		   for (int index = 0; index < data.length; index++) {
-			  if (data[index] == true) {
-			  	result = true;}
-			  }
-		   return result;
+		 for (int index = 0; index < data.length; index++) {
+            if (data[index]!=data[0])
+            	return result;
 
+		 }
+       return result = true;
 	 }
-
  }
+
+
+
+
